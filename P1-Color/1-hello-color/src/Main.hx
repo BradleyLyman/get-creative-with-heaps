309457d6c2ad37647bd9f2f3ae8c13fa759b3f10
Bradley Lyman <lyman.brad3211@gmail.com>
2020-06-24 21:44:09 -0700

    comment 1-hello-color
    because:
    - the existing comments were fairly sparse
    - the existing comments were not very idomatic (formatting, etc..)
    - the first demo should have a few extra comments to introduce ideas

--- P1-Color/1-hello-color/src/Main.hx
+++ P1-Color/1-hello-color/src/Main.hx
@@ -1,32 +1,30 @@
 class Main extends hxd.App {
 
-    /**
-     * Graphics is used for immediate-mode 2d rendering.
-     */
+    /* Used for drawing 2d shapes on screen. */
     var graphics : h2d.Graphics;
 
-    /**
-     * How much of the screen should the foreground rectangle occupy.
-     */
+    /* How much of the screen should the foreground rectange occupy */
     var sizeRatio : Float;
 
-    /**
-     * The foreground hue.
-     */
+    /* The foreground hue. The H in HSL for the foreground color */
     var foregroundHue : Float;
 
-    /**
-     * A reference to the main window. Useful for quickly checking the mouse
-     * position.
-     */
+    /* A reference to the main window, used to get the mouse position. */
     var window : hxd.Window;
 
-
+    /**
+        Initialize the demo. This is automatically called by Heaps when
+        the window is ready.
+    **/
     override function init() {
         graphics = new h2d.Graphics(s2d);
         window = hxd.Window.getInstance();
     }
 
+    /**
+        Update the contents of the screen.
+        This is automatically called by Heaps before each frame.
+    **/
     override function update(dt: Float) {
         // The size ratio is controlled by the mouse's X position
         sizeRatio = Math.abs((window.mouseX * 2 / s2d.width) - 1.0);
@@ -35,10 +33,22 @@ class Main extends hxd.App {
         final normalized_y = window.mouseY / s2d.height;
         foregroundHue = normalized_y * (Math.PI * 2);
 
+        // Lines and rectangles will accumulate in the graphics object unless
+        // it's cleared.
         graphics.clear();
+
+        // draw the background first
         drawBackground();
+
+        // then the foreground
         drawForeground();
     }
+
+    /**
+        The background is a rectangle which fills the entire screen.
+        The hue is offset by Math.PI to select a color on the opposite side
+        of the color wheel from the foreground rectangle.
+    **/
     private function drawBackground() {
         // The background uses the inverted hue
         graphics.beginFill(colorFor(foregroundHue - Math.PI));
@@ -46,6 +56,11 @@ class Main extends hxd.App {
         graphics.endFill();
     }
 
+    /**
+        The foreground is a rectangle which fills a section of the screen
+        controlled b ythe mouse.
+        The emptyRatio and sizeRatio are used to center the rectangle.
+    **/
     private function drawForeground() {
         final emptyRatio = 1.0 - sizeRatio;
         final borderWidth = emptyRatio / 2.0;
@@ -59,13 +74,17 @@ class Main extends hxd.App {
         graphics.endFill();
     }
 
+    /**
+        A function which transforms a color represented as hue, saturation,
+        and brightness to a packed 32-bit RGB Int.
+    **/
     private function colorFor(
         hue: Float,
         saturation: Float = 0.8,
-        lightness: Float = 0.5
+        brightness: Float = 0.5
     ) : Int {
         final vec = new h3d.Vector();
-        vec.makeColor(hue, saturation, lightness);
+        vec.makeColor(hue, saturation, brightness);
         return vec.toColor();
     }
 
