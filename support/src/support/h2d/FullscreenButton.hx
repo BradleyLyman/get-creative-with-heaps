338821010729a70465a22a135ea5569a07442c65
Bradley Lyman <lyman.brad3211@gmail.com>
2020-07-12 14:06:21 -0700

    update fullscreen button

--- support/src/support/h2d/FullscreenButton.hx
+++ support/src/support/h2d/FullscreenButton.hx
@@ -1,5 +1,8 @@
 package support.h2d;
 
+import support.linAlg2d.Quad;
+import support.color.RGBA;
+import h2d.Drawable;
 import hxd.Window.DisplayMode;
 
 /**
@@ -9,15 +12,13 @@ import hxd.Window.DisplayMode;
     The button image is dynamically generated so there's no need to worry about
     resources when using this button.
 **/
-class FullscreenButton {
-  /* The button's root establishes the origin for all coordinates. */
-  var root : h2d.Object;
+class FullscreenButton extends Drawable {
 
   /* The clickable region of the button. */
   var clickable : h2d.Interactive;
 
   /* The button's visual component. */
-  var graphics : h2d.Graphics;
+  var fastQuads : FastQuads;
 
   /* A reference to the window, used for events and getting dimensions */
   var window : hxd.Window;
@@ -33,9 +34,9 @@ class FullscreenButton {
       Resizing and redrawing are handled automatically.
   **/
   public function new(parent: h2d.Scene) {
-    root = new h2d.Object(parent);
-    graphics = new h2d.Graphics(root);
-    clickable = new h2d.Interactive(1, 1, root);
+    super(parent);
+    fastQuads = new FastQuads(this);
+    clickable = new h2d.Interactive(1, 1, this);
     clickable.onClick = onClick;
 
     window = hxd.Window.getInstance();
@@ -49,11 +50,11 @@ class FullscreenButton {
       Compute the button's dimensions based on the window's position.
   **/
   private function updateDimensions() {
-    final size = Math.max(Math.min(window.width, window.height) / 20, 20);
+    final size = Math.max(Math.min(window.width, window.height) / 24, 24);
     dimensions = {
       size: size,
       lineSize: Math.max(size / 24, 1.0),
-      offset: Math.max(size / 6, 2)
+      offset: Math.max(size / 6, 2.0)
     };
   }
 
@@ -65,10 +66,10 @@ class FullscreenButton {
     clickable.width = dimensions.size;
     clickable.height = dimensions.size;
 
-    root.x = window.width - (dimensions.size*2);
-    root.y = window.height - (dimensions.size*2);
-    graphics.x = dimensions.size / 2;
-    graphics.y = dimensions.size / 2;
+    this.x = window.width - (dimensions.size*2);
+    this.y = window.height - (dimensions.size*2);
+    fastQuads.x = dimensions.size / 2;
+    fastQuads.y = dimensions.size / 2;
 
     render();
   }
@@ -89,16 +90,16 @@ class FullscreenButton {
       Render the button using the computed dimensions.
   **/
   private function render() {
-    graphics.clear();
+    fastQuads.clear();
 
     // draw the background rect
-    graphics.beginFill(0xFFFFFF, 0.3);
-    graphics.drawRoundedRect(
-      -dimensions.size/2, -dimensions.size/2, // position
-      dimensions.size, dimensions.size,       // size
-      dimensions.offset                       // corner radius
+    fastQuads.addQuad(
+      Quad.aligned(
+        -dimensions.size/2, -dimensions.size/2,
+        dimensions.size/2, dimensions.size/2
+      ),
+      new RGBA(1, 1, 1, 0.3)
     );
-    graphics.endFill();
 
     final offset = dimensions.offset;
 
@@ -108,25 +109,26 @@ class FullscreenButton {
     final right = (dimensions.size/2) - offset;
 
     // draw arrows
-    graphics.lineStyle(dimensions.lineSize, 0xFFFFFF, 0.95);
+    final lines = fastQuads.newTurtle();
+    lines.lineWidth = dimensions.lineSize;
 
     // draw radial lines
-    graphics.moveTo( offset,  offset); graphics.lineTo(right, bottom);
-    graphics.moveTo(-offset,  offset); graphics.lineTo(left,  bottom);
-    graphics.moveTo( offset, -offset); graphics.lineTo(right, top);
-    graphics.moveTo(-offset, -offset); graphics.lineTo(left,  top);
+    lines.moveTo( offset,  offset).lineTo(right, bottom);
+    lines.moveTo(-offset,  offset).lineTo(left,  bottom);
+    lines.moveTo( offset, -offset).lineTo(right, top);
+    lines.moveTo(-offset, -offset).lineTo(left,  top);
 
     // draw arrowheads
-    graphics.moveTo(left, top); graphics.lineTo(-offset, top);
-    graphics.moveTo(left, top); graphics.lineTo(left, -offset);
+    lines.moveTo(left, top).lineTo(-offset, top);
+    lines.moveTo(left, top).lineTo(left, -offset);
 
-    graphics.moveTo(right, top); graphics.lineTo(offset, top);
-    graphics.moveTo(right, top); graphics.lineTo(right, -offset);
+    lines.moveTo(right, top).lineTo(offset, top);
+    lines.moveTo(right, top).lineTo(right, -offset);
 
-    graphics.moveTo(left, bottom); graphics.lineTo(-offset, bottom);
-    graphics.moveTo(left, bottom); graphics.lineTo(left, offset);
+    lines.moveTo(left, bottom).lineTo(-offset, bottom);
+    lines.moveTo(left, bottom).lineTo(left, offset);
 
-    graphics.moveTo(right, bottom); graphics.lineTo(offset, bottom);
-    graphics.moveTo(right, bottom); graphics.lineTo(right, offset);
+    lines.moveTo(right, bottom).lineTo(offset, bottom);
+    lines.moveTo(right, bottom).lineTo(right, offset);
   }
 }
