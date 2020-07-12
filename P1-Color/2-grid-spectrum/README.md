12a638dd37d5a615c057539bde8aa41cb8ffddeb
Bradley Lyman <lyman.brad3211@gmail.com>
2020-07-12 14:27:35 -0700

    update 2-grid-spectrum to use the support library

--- P1-Color/2-grid-spectrum/README.md
+++ P1-Color/2-grid-spectrum/README.md
@@ -1,8 +1,9 @@
-# Grid Spectrum
+# [Grid Spectrum](http://bradlyman.github.io/get-creative-with-heaps/P1-Color/2-grid-spectrum/)
 
-![Low Resolution Grid](https://github.com/BradLyman/learn_you_a_heaps/blob/master/P1-Color/2-grid-spectrum/Screenshot1.png)
-
-![High Resolution Grid](https://github.com/BradLyman/learn_you_a_heaps/blob/master/P1-Color/2-grid-spectrum/Screenshot2.png)
+<a href="http://bradlyman.github.io/get-creative-with-heaps/P1-Color/2-grid-spectrum/"
+   title="Live Demo" >
+  ![demoscreenshot](https://bradlyman.github.io/get-creative-with-heaps/P1-Color/2-grid-spectrum/Screenshot.png)
+</a>
 
 ## Run The Demo
 
@@ -14,14 +15,3 @@
 
 * The mouse's Y coordinate controls the number of colors top to bottom
 * The mouse's X coordinate controls the number of colors left to right
-
-# Overview
-
-Build an intuition for HSB Colors by dividing the entire screen into a color grid.
-
-## Implementation Notes
-
-The screen is divided into a grid of rectangles. The x coordinate of the left
-of each rectangle is scaled from [0, 2π] to represent the hue for that
-rectangle. The Y coordinate of the top of the rectangle is scaled from [0, 1] to control 
-the brightness for that rectangle.
--- P1-Color/2-grid-spectrum/src/Main.hx
+++ P1-Color/2-grid-spectrum/src/Main.hx
@@ -1,107 +1,82 @@
+import support.linAlg2d.Quad;
+import support.color.HSL;
+import support.h2d.FastQuads;
+
 class Main extends hxd.App {
 
-    /* Used for drawing 2d shapes on screen. */
-    var graphics : h2d.Graphics;
+  /* Quickly draw a lot of quads on screen */
+  var quads : FastQuads;
 
-    /* A reference to the main window, used to get the mouse position. */
-    var window : hxd.Window;
+  /* How many rows and columns to divide the screen into. */
+  var columnCount : Int = 3;
+  var rowCount : Int = 3;
 
-    /* How many rows and columns to divide the screen into. */
-    var columnCount : Int = 3;
-    var rowCount : Int = 3;
+  /* Initialize the demo. Called automatically by Heaps. */
+  override function init() {
+    quads = new FastQuads(s2d);
+    new support.h2d.FullscreenButton(s2d);
+  }
 
-    /* Initialize the demo. Called automatically by Heaps. */
-    override function init() {
-        graphics = new h2d.Graphics(s2d);
-        window = hxd.Window.getInstance();
-        new support.h2d.FullscreenButton(s2d);
+  /* Update the screen. Called before each frame. */
+  override function update(dt: Float) {
+    quads.clear();
+    updateSteps();
+    drawSpectrumGrid();
+  }
+
+  /**
+      Use the mouse coordinates to calculate new horizontal and vertical
+      step values. These are used to divide the screen into rows and columns.
+  **/
+  private function updateSteps() {
+    final normalized = {
+        x: s2d.mouseX / s2d.width,
+        y: s2d.mouseY / s2d.height
+    };
+
+    // the number of columns can range from [3, 360]
+    columnCount = 3 + Math.floor(normalized.x * 357);
+
+    // the number of rows can range from [3, 100]
+    rowCount = 3 + Math.floor(normalized.y * 97);
+  }
+
+  /**
+      Draw the spectrum grid by dividing the screen into rows and colums.
+      Each column is a different hue. Each row is a different value, aka
+      brightness.
+  **/
+  private function drawSpectrumGrid() {
+    final width = s2d.width / columnCount;
+    final height = s2d.height / rowCount;
+
+    for (x in 0...columnCount) {
+      for (y in 0...rowCount) {
+        // map the x and y coordinates into the range [0, 1]
+        final normalized_x = x / columnCount;
+        final normalized_y = y / rowCount;
+
+        // map the normalized coordinates into screen coordinates
+        // aka pixels
+        final left = normalized_x * s2d.width;
+        final top = normalized_y * s2d.height;
+
+        // map the normalized coord from [0, 1] to [0, 360]
+        final hue = normalized_x * 360;
+
+        // map the normalized coord from [0, 1] to [0, 1]
+        // But, select the value from the midpoint of the rectangle,
+        // instead of the top edge. (it looks better)
+        final midpoint_step = 0.5 / rowCount;
+        final value = normalized_y + midpoint_step;
+
+        quads.addQuad(
+          Quad.aligned(top, left, top + height, left + width),
+          new HSL(hue, 0.8, value).toRGBA()
+        );
+      }
     }
+  }
 
-    /* Update the screen. Called before each frame. */
-    override function update(dt: Float) {
-        graphics.clear(); // clear the grid from the previous frame
-        updateSteps();
-        drawSpectrumGrid();
-    }
-
-    /**
-        Use the mouse coordinates to calculate new horizontal and vertical
-        step values. These are used to divide the screen into rows and columns.
-    **/
-    private function updateSteps() {
-        final normalized = {
-            x: window.mouseX / s2d.width,
-            y: window.mouseY / s2d.height
-        };
-
-        // the number of columns can range from [3, 50]
-        columnCount = 3 + Math.floor(normalized.x * 47);
-
-        // the number of rows can range from [3, 20]
-        rowCount = 3 + Math.floor(normalized.y * 17);
-    }
-
-    /**
-        Draw the spectrum grid by dividing the screen into rows and colums.
-        Each column is a different hue. Each row is a different value, aka
-        brightness.
-    **/
-    private function drawSpectrumGrid() {
-        // compute the position and color for each rectangle in the grid
-        final grid = [
-            for (x in 0...columnCount) {
-                for (y in 0...rowCount) {
-                    // map the x and y coordinates into the range [0, 1]
-                    final normalized_x = x / columnCount;
-                    final normalized_y = y / rowCount;
-
-                    // map the normalized coordinates into screen coordinates
-                    // aka pixels
-                    final left_position = normalized_x * s2d.width;
-                    final top_position = normalized_y * s2d.height;
-
-                    // map the normalized coord from [0, 1] to [0, TWO_PI]
-                    final hue =  normalized_x * (Math.PI*2);
-
-                    // map the normalized coord from [0, 1] to [0, 1]
-                    // But, select the value from the midpoint of the rectangle,
-                    // instead of the top edge. (it looks better)
-                    final midpoint_step = 0.5 / rowCount;
-                    final value = normalized_y + midpoint_step;
-
-                    // emit the actual rectangle
-                    {
-                        left: left_position,
-                        top: top_position,
-                        hue: hue,
-                        value: value
-                    };
-                }
-            }
-        ];
-
-        final width = s2d.width / columnCount;
-        final height = s2d.height / rowCount;
-
-        // draw each rectangle in the grid
-        for (rect in grid) {
-            graphics.beginFill(colorFor(rect.hue, 0.8, rect.value));
-            graphics.drawRect(rect.left, rect.top, width, height);
-            graphics.endFill();
-        }
-    }
-
-    private function colorFor(
-        hue: Float,
-        saturation: Float = 0.8,
-        brightness: Float = 0.5
-    ) : Int {
-        final vec = new h3d.Vector();
-        vec.makeColor(hue, saturation, brightness);
-        return vec.toColor();
-    }
-
-    static function main() {
-        new Main();
-    }
+  static function main() { new Main(); }
 }
