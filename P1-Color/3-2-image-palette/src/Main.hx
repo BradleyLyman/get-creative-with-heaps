e7605859369e33d60a817cea9657a5070a33e200
Bradley Lyman <lyman.brad3211@gmail.com>
2020-07-12 15:19:03 -0700

    clean up formatting

--- P1-Color/3-2-image-palette/src/Main.hx
+++ P1-Color/3-2-image-palette/src/Main.hx
@@ -1,99 +1,92 @@
 import hxd.Key;
 import h3d.Vector;
-import haxe.io.Bytes;
-import h2d.Object;
 
 using PixelTools;
 
 class Main extends hxd.App {
 
-    /** A collection of image data which can be used onscreen **/
-    var images : Array<hxd.Pixels>;
+  /** A collection of image data which can be used onscreen **/
+  var images : Array<hxd.Pixels>;
 
-    /** The index into images[] for the currently displayed picture */
-    var imageIndex : Int;
+  /** The index into images[] for the currently displayed picture */
+  var imageIndex : Int;
 
-    /** A graphical object which knows how to paint bitmap data */
-    var displayed : h2d.Bitmap;
+  /** A graphical object which knows how to paint bitmap data */
+  var displayed : h2d.Bitmap;
 
-    override function init() {
-        imageIndex = 0;
-        images = [
-            hxd.Res.landscape1.toBitmap().getPixels(),
-            hxd.Res.landscape2.toBitmap().getPixels(),
-            hxd.Res.landscape3.toBitmap().getPixels(),
-        ];
-        displayed = new h2d.Bitmap(h2d.Tile.fromPixels(images[0]), s2d);
+  override function init() {
+    imageIndex = 0;
+    images = [
+      hxd.Res.landscape1.toBitmap().getPixels(),
+      hxd.Res.landscape2.toBitmap().getPixels(),
+      hxd.Res.landscape3.toBitmap().getPixels(),
+    ];
+    displayed = new h2d.Bitmap(h2d.Tile.fromPixels(images[0]), s2d);
 
-        hxd.Window.getInstance().addEventTarget(onEvent);
-        onResize();
+    hxd.Window.getInstance().addEventTarget(onEvent);
+    onResize();
 
-        new support.h2d.FullscreenButton(s2d);
+    new support.h2d.FullscreenButton(s2d);
+  }
+
+  /**
+      Handle Window events.
+      When the mouse is clicked show the next image in our list.
+      When a number key is pressed, replace the pixels according to a sort
+      operation.
+  **/
+  function onEvent(event: hxd.Event) {
+    switch (event.kind) {
+      case ERelease: // do nothing
+        showNextImage();
+      case EKeyUp:
+        onCharUp(event.keyCode);
+      default: // do nothing
     }
+  }
 
-    /**
-        Handle Window events.
-        When the mouse is clicked show the next image in our list.
-        When a number key is pressed, replace the pixels according to a sort
-        operation.
-    **/
-    function onEvent(event: hxd.Event) {
-        switch (event.kind) {
-            case ERelease: // do nothing
-                showNextImage();
-            case EKeyUp:
-                onCharUp(event.keyCode);
-            default: // do nothing
-        }
-    }
+  /* Handle a number key release. */
+  function onCharUp(code: Int) {
+    final newPixels : hxd.Pixels = switch (code) {
+      case Key.NUMBER_1: images[imageIndex].toSorted(orderByHue);
+      case Key.NUMBER_2: images[imageIndex].toSorted(orderBySaturation);
+      case Key.NUMBER_3: images[imageIndex].toSorted(orderByLightness);
+      default: images[imageIndex];
+    };
+    displayed.tile = h2d.Tile.fromPixels(newPixels);
+  }
 
-    /**
-        Handle a number key release.
-    **/
-    function onCharUp(code: Int) {
-        final newPixels : hxd.Pixels = switch (code) {
-                case Key.NUMBER_1:
-                    images[imageIndex].toSorted(orderByHue);
-                case Key.NUMBER_2:
-                    images[imageIndex].toSorted(orderBySaturation);
-                case Key.NUMBER_3:
-                    images[imageIndex].toSorted(orderByLightness);
-                default: images[imageIndex];
-            };
-        displayed.tile = h2d.Tile.fromPixels(newPixels);
+  /**
+      Update the current image index. If it's too big, wrap around to the
+      first image again.
+  **/
+  function showNextImage() {
+    imageIndex += 1;
+    if (imageIndex >= images.length) {
+      imageIndex = 0;
     }
+    displayed.tile = h2d.Tile.fromPixels(images[imageIndex]);
+  }
 
-    /**
-        Update the current image index. If it's too big, wrap around to the
-        first image again.
-    **/
-    function showNextImage() {
-        imageIndex += 1;
-        if (imageIndex >= images.length) {
-            imageIndex = 0;
-        }
-        displayed.tile = h2d.Tile.fromPixels(images[imageIndex]);
-    }
+  function orderByHue(a: Vector, b: Vector) : Bool {
+    return a.toColorHSL().x > b.toColorHSL().x;
+  }
 
-    function orderByHue(a: Vector, b: Vector) : Bool {
-        return a.toColorHSL().x > b.toColorHSL().x;
-    }
+  function orderBySaturation(a: Vector, b: Vector) : Bool {
+    return a.toColorHSL().y > b.toColorHSL().y;
+  }
 
-    function orderBySaturation(a: Vector, b: Vector) : Bool {
-        return a.toColorHSL().y > b.toColorHSL().y;
-    }
+  function orderByLightness(a: Vector, b: Vector) : Bool {
+    return a.toColorHSL().z > b.toColorHSL().z;
+  }
 
-    function orderByLightness(a: Vector, b: Vector) : Bool {
-        return a.toColorHSL().z > b.toColorHSL().z;
-    }
+  override function onResize() {
+    displayed.width = s2d.width;
+    displayed.height = s2d.height;
+  }
 
-    override function onResize() {
-        displayed.width = s2d.width;
-        displayed.height = s2d.height;
-    }
-
-    static function main() {
-        hxd.Res.initEmbed();
-        new Main();
-    }
+  static function main() {
+    hxd.Res.initEmbed();
+    new Main();
+  }
 }
--- P1-Color/3-2-image-palette/src/PixelTools.hx
+++ P1-Color/3-2-image-palette/src/PixelTools.hx
@@ -1,52 +1,63 @@
 import haxe.io.Bytes;
 
 class PixelTools {
+  /**
+      Treat a Pixels object like a list of sortable values. Sort them according
+      to the provided function and return a pixels object with the same
+      width and height.
 
-    /**
-        @param pixels
-            The pixels to read. No changes happen to this object.
-        @param greaterThan
-            A function which compares two h3d.Vectors which represent the pixel
-            a pixel Color.
-        @return
-            A new pixel instance with all values reorganized according to the
-            greaterThan function.
-    **/
-    public static function toSorted(
-        pixels: hxd.Pixels,
-        greaterThan: (h3d.Vector, h3d.Vector) -> Bool
-    ) : hxd.Pixels {
-        pixels.convert(hxd.PixelFormat.BGRA);
-        var pixelVec3ds = pixels.toVector().map(i -> h3d.Vector.fromColor(i));
+      @param pixels
+          The pixels to read. No changes happen to this object.
+      @param greaterThan
+          A function which compares two h3d.Vectors which represent the pixel
+          a pixel Color.
+      @return
+          A new pixel instance with all values reorganized according to the
+          greaterThan function.
+  **/
+  public static function toSorted(
+    pixels: hxd.Pixels,
+    greaterThan: (h3d.Vector, h3d.Vector) -> Bool
+  ) : hxd.Pixels {
+    pixels.convert(hxd.PixelFormat.BGRA);
 
-        pixelVec3ds.sort((x, y) -> {
-            if (greaterThan(x, y)) {
-                return 1;
-            } else {
-                return -1;
-            }
-        });
+    var pixelVec3ds = pixels.toVector().map(i -> h3d.Vector.fromColor(i));
 
-        return new hxd.Pixels(
-            pixels.width,
-            pixels.height,
-            pixelIntsToBytes(pixelVec3ds.map(v -> v.toColor())),
-            hxd.PixelFormat.BGRA
-        );
+    pixelVec3ds.sort((x, y) -> {
+      if (greaterThan(x, y)) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+
+    return new hxd.Pixels(
+      pixels.width,
+      pixels.height,
+      pixelIntsToBytes(pixelVec3ds.map(v -> v.toColor())),
+      hxd.PixelFormat.BGRA
+    );
+  }
+
+  /**
+      Convert a Vector of 32 bit ints into a Byte collection.
+
+      This may seem like an odd approach (it did for me!). But the
+      haxe.ds.Vector is a platform-specific type and it's not guaranteed to
+      be a contigious block of memory. Hence the need to copy int for int into
+      the returned bytes.
+
+      This does mean that careful use of #if conditionals could yield a perf
+      improvement by avoiding the uneeded copy and loop. But there's little
+      evidince that *this* is a bottleneck which needs optimizing.
+  **/
+  private static function pixelIntsToBytes(ints: haxe.ds.Vector<Int>) : Bytes {
+    final bytes = Bytes.alloc(ints.length * 4);
+    var offset = 0;
+    for (value in ints) {
+      bytes.setInt32(offset, value);
+      offset += 4;
     }
-
-    /**
-        Convert a Vector of 32 bit ints into a Byte collection.
-    **/
-    private static function pixelIntsToBytes(
-        ints: haxe.ds.Vector<Int>
-    ) : Bytes {
-        final bytes = Bytes.alloc(ints.length * 4);
-        var offset = 0;
-        for (value in ints) {
-            bytes.setInt32(offset, value);
-            offset += 4;
-        }
-        return bytes;
-    }
+    return bytes;
+  }
 }
