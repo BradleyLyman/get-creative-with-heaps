9ea3ca9278b4b4a7eae1723391e1c58282f3463c
Bradley Lyman <lyman.brad3211@gmail.com>
2020-07-13 01:01:48 -0700

    switch to using ui controls in the support lib

--- P3-Harmonics/2-lissajous/src/Main.hx
+++ P3-Harmonics/2-lissajous/src/Main.hx
@@ -1,6 +1,8 @@
 import support.color.HSL;
 import support.h2d.Plot;
 import support.h2d.FullscreenButton;
+import support.h2d.ui.BasicControls;
+import support.h2d.ui.NamedSlider;
 import support.linAlg2d.Interval;
 
 class Main extends hxd.App {
@@ -10,7 +12,7 @@ class Main extends hxd.App {
   var tvals: haxe.ds.Vector<Float>;
   var time: Float = 0.0;
 
-  var cp : BasicControls;
+  var controls : BasicControls;
   var f1 : NamedSlider;
   var f2 : NamedSlider;
 
@@ -25,33 +27,6 @@ class Main extends hxd.App {
     onResize();
   }
 
-  private function setupControlPanel() {
-    final notoSans = hxd.Res.fonts.NotoSans16.toFont();
-    cp = new BasicControls(s2d, notoSans);
-    cp.backgroundColor = new HSL(260, 0.25, 0.25, 0.75);
-
-    cp.addText("== signal frequencies ==");
-    f1 = cp.addSlider("f1", 0.5, 5);
-    f2 = cp.addSlider("f2", 0.5, 5);
-    f1.value = 2.1;
-    f2.value = 1.3;
-
-    cp.addText("== sample domain ==");
-    final divisions = cp.addSlider("subdivisions", 20, 150);
-    divisions.value = 100;
-    divisions.onChange = () -> {
-      tvals = domain.subdivide(Math.round(divisions.value));
-    };
-    divisions.onChange();
-
-    final max = cp.addSlider("end", Math.PI, Math.PI * 4);
-    max.value = Math.PI * 2;
-    max.onChange = () -> {
-      domain.end = max.value;
-      tvals = domain.subdivide(Math.round(divisions.value));
-    };
-  }
-
   override function onResize() {
     final side = Math.min(s2d.width, s2d.height) * 0.95;
     plot.resize(side, side);
@@ -61,9 +36,13 @@ class Main extends hxd.App {
 
   override function update(dt: Float) {
     final speed = 0.5 * (Math.PI / 4);
-    if (!cp.visible) time += dt*speed;
-    final xSignal = (t: Float) -> Math.cos(time + t*f1.value);
-    final ySignal = (t: Float) -> Math.sin(time + t*f2.value);
+
+    // pause when the control panel is visible
+    if (!controls.visible) time += dt*speed;
+
+    // plot the two functions with their respective frequencies
+    final xSignal = (t: Float) -> Math.cos((time + t)*f1.value);
+    final ySignal = (t: Float) -> Math.sin((time + t)*f2.value);
 
     plot.clear();
     final maxLenTurtle = new MaxLengthTurtle(plot.turtle, 0.5);
@@ -74,6 +53,33 @@ class Main extends hxd.App {
           .lineTo(xSignal(j), ySignal(j));
       }
     }
+  }
+
+  private function setupControlPanel() {
+    final notoSans = hxd.Res.fonts.NotoSans16.toFont();
+    controls = new BasicControls(s2d, notoSans);
+    controls.backgroundColor = new HSL(260, 0.25, 0.25, 0.75);
+
+    controls.addText("== signal frequencies ==");
+    f1 = controls.addSlider("f1", 0.5, 5);
+    f2 = controls.addSlider("f2", 0.5, 5);
+    f1.value = 2.1;
+    f2.value = 1.3;
+
+    controls.addText("== sample domain ==");
+    final divisions = controls.addSlider("subdivisions", 20, 150);
+    divisions.value = 100;
+    divisions.onChange = () -> {
+      tvals = domain.subdivide(Math.round(divisions.value));
+    };
+    divisions.onChange();
+
+    final max = controls.addSlider("end", Math.PI, Math.PI * 4);
+    max.value = Math.PI * 2;
+    max.onChange = () -> {
+      domain.end = max.value;
+      tvals = domain.subdivide(Math.round(divisions.value));
+    };
   }
 
   static function main() {
